bdfbac3e4a3031da4e61637dc2719ae6384d0745
Jayant Sarwale <jayantsarwale@gmail.com>
2024-10-15 19:08:51 +0530

    New Files and folder added for our website

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, Worl!</p>"
+@app.route("/home")
+def homepage():
+    return render_template('home.html')
+
+@app.route('/reg')
+def register():
+    return render_template('reg.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
